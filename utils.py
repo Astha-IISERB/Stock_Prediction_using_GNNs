0556f4bf79f446c6c64d6650677c1e02459439ee
a22063_astha <130688373+Astha-IISERB@users.noreply.github.com>
2025-11-20 08:33:59 +0530

    utils.py

---
+++ utils.py
@@ -0,0 +1,249 @@
+import numpy as np
+import random
+import pickle
+import torch
+import torch.nn.functional as F
+
+def normalize_digraph(A):
+    Dl = np.sum(A, 0)
+    num_node = A.shape[0]
+    Dn = np.zeros((num_node, num_node))
+    for i in range(num_node):
+        if Dl[i] > 0:
+            Dn[i, i] = Dl[i]**(-1)
+    AD = np.dot(A, Dn)
+    return AD
+
+
+def normalize_undigraph(A):
+    Dl = np.sum(A, 0)
+    num_node = A.shape[0]
+    Dn = np.zeros((num_node, num_node))
+    for i in range(num_node):
+        if Dl[i] > 0:
+            Dn[i, i] = Dl[i]**(-0.5)
+    DAD = np.dot(np.dot(Dn, A), Dn)
+
+class Graph():
+    """ The Graph to model the skeletons extracted by the openpose
+
+    Args:
+        strategy (string): must be one of the follow candidates
+        - uniform: Uniform Labeling
+        - distance: Distance Partitioning
+        - spatial: Spatial Configuration
+        For more information, please refer to the section 'Partition Strategies'
+            in our paper (https://arxiv.org/abs/1801.07455).
+
+        layout (string): must be one of the follow candidates
+        - openpose: Is consists of 18 joints. For more information, please
+            refer to https://github.com/CMU-Perceptual-Computing-Lab/openpose#output
+        - ntu-rgb+d: Is consists of 25 joints. For more information, please
+            refer to https://github.com/shahroudy/NTURGB-D
+
+        max_hop (int): the maximal distance between two connected nodes
+        dilation (int): controls the spacing between the kernel points
+
+    """
+
+    def __init__(self,
+                 market='NIFTY',
+                 strategy='uniform',
+                 relation_path=None):
+        self.get_edge(market, relation_path)
+        self.get_adjacency(strategy)
+
+    def __str__(self):
+        return self.A
+
+    def get_edge(self, market, relation_path):
+        if market == 'NIFTY':
+            self.num_node = 171
+            self_link = np.eye(self.num_node)
+            raw_relation = np.load(relation_path)
+            
+            self.relation = raw_relation[:,:,:]
+            _relation = np.sum(self.relation, axis=2)
+            _relation[_relation>=1] = 1
+
+            self.edge = self_link + _relation
+
+        else:
+            raise ValueError("Do Not Exist This Layout.")
+
+    def get_adjacency(self, strategy):
+        adjacency = self.edge
+
+        if strategy == 'uniform':
+            normalize_adjacency = normalize_digraph(adjacency)
+            A = np.zeros((1, self.num_node, self.num_node))
+            A[0] = normalize_adjacency
+            self.A = A
+        else:
+            raise ValueError("Do Not Exist This Strategy")
+
+def auto_pading(data_numpy, size, random_pad=False):
+    C, T, V, M = data_numpy.shape
+    if T < size:
+        begin = random.randint(0, size - T) if random_pad else 0
+        data_numpy_paded = np.zeros((C, size, V, M))
+        data_numpy_paded[:, begin:begin + T, :, :] = data_numpy
+        return data_numpy_paded
+    else:
+        return data_numpy
+
+
+def random_choose(data_numpy, size, auto_pad=True):
+    # input: C,T,V,M
+    C, T, V, M = data_numpy.shape
+    if T == size:
+        return data_numpy
+    elif T < size:
+        if auto_pad:
+            return auto_pading(data_numpy, size, random_pad=True)
+        else:
+            return data_numpy
+    else:
+        begin = random.randint(0, T - size)
+        return data_numpy[:, begin:begin + size, :, :]
+
+
+def random_move(data_numpy,
+                angle_candidate=[-10., -5., 0., 5., 10.],
+                scale_candidate=[0.9, 1.0, 1.1],
+                transform_candidate=[-0.2, -0.1, 0.0, 0.1, 0.2],
+                move_time_candidate=[1]):
+    # input: C,T,V,M
+    C, T, V, M = data_numpy.shape
+    move_time = random.choice(move_time_candidate)
+    node = np.arange(0, T, T * 1.0 / move_time).round().astype(int)
+    node = np.append(node, T)
+    num_node = len(node)
+
+    A = np.random.choice(angle_candidate, num_node)
+    S = np.random.choice(scale_candidate, num_node)
+    T_x = np.random.choice(transform_candidate, num_node)
+    T_y = np.random.choice(transform_candidate, num_node)
+
+    a = np.zeros(T)
+    s = np.zeros(T)
+    t_x = np.zeros(T)
+    t_y = np.zeros(T)
+
+    # linspace
+    for i in range(num_node - 1):
+        a[node[i]:node[i + 1]] = np.linspace(
+            A[i], A[i + 1], node[i + 1] - node[i]) * np.pi / 180
+        s[node[i]:node[i + 1]] = np.linspace(S[i], S[i + 1],
+                                             node[i + 1] - node[i])
+        t_x[node[i]:node[i + 1]] = np.linspace(T_x[i], T_x[i + 1],
+                                               node[i + 1] - node[i])
+        t_y[node[i]:node[i + 1]] = np.linspace(T_y[i], T_y[i + 1],
+                                               node[i + 1] - node[i])
+
+    theta = np.array([[np.cos(a) * s, -np.sin(a) * s],
+                      [np.sin(a) * s, np.cos(a) * s]])
+
+    # perform transformation
+    for i_frame in range(T):
+        xy = data_numpy[0:2, i_frame, :, :]
+        new_xy = np.dot(theta[:, :, i_frame], xy.reshape(2, -1))
+        new_xy[0] += t_x[i_frame]
+        new_xy[1] += t_y[i_frame]
+        data_numpy[0:2, i_frame, :, :] = new_xy.reshape(2, V, M)
+
+    return data_numpy
+
+
+class Feeder(torch.utils.data.Dataset):
+    """ Feeder for skeleton-based action recognition
+    Arguments:
+        data_path: the path to '.npy' data, the shape of data should be (N, C, T, V)
+        price_path: the path to '.npy' data, the closing price
+        label_path: the path to label
+        random_choose: If true, randomly choose a portion of the input sequence
+        random_shift: If true, randomly pad zeros at the begining or end of sequence
+        window_size: The length of the output sequence
+        normalization: If true, normalize input sequence
+        debug: If true, only use the first 100 samples
+    """
+
+    def __init__(self,
+                 data_path,
+                 price_path,
+                 label_path,
+                 random_choose=False,
+                 random_move=False,
+                 window_size=-1,
+                 debug=False,
+                 mmap=True):
+        self.debug = debug
+        self.data_path = data_path
+        self.price_path = price_path
+        self.label_path = label_path
+        self.random_choose = random_choose
+        self.random_move = random_move
+        self.window_size = window_size
+
+        self.load_data(mmap)
+
+    def load_data(self, mmap):
+        # data: N C V T
+
+        # load label
+        with open(self.label_path, 'rb') as f:
+            self.label = pickle.load(f)
+
+        # load data
+        if mmap:
+            self.data = np.load(self.data_path, mmap_mode='r')
+            self.closing_price = np.load(self.price_path, mmap_mode='r')
+        else:
+            self.data = np.load(self.data_path)
+            self.closing_price = np.load(self.price_path)
+            
+        if self.debug:
+            self.label = self.label[0:200]
+            self.data = self.data[0:200]
+            self.closing_price = self.closing_price[:200]
+            # self.sample_name = self.sample_name[0:100]
+        self.N, self.C, self.T, self.V = self.data.shape
+
+    def __len__(self):
+        return len(self.label)
+
+    def __getitem__(self, index):
+        # get data
+        data_numpy = np.array(self.data[index])
+        label = np.array(self.label[index])
+        closing_price = np.array(self.closing_price[index])
+        
+        # processing
+        if self.random_choose:
+            data_numpy = random_choose(data_numpy, self.window_size)
+        elif self.window_size > 0:
+            data_numpy = auto_pading(data_numpy, self.window_size)
+        if self.random_move:
+            data_numpy = random_move(data_numpy)
+
+        return data_numpy, closing_price, label
+
+def calc_return_ratio(result, label, k):
+    # calculate the return-ration of the top k stocks
+    bt_k = 1.0
+    for i in range(result.shape[0]):
+      rank_pre = np.argsort(result[i])
+
+      pre_topk = set()
+      for j in range(1, result.shape[1] + 1):
+          cur_rank = rank_pre[-1 * j]
+          if len(pre_topk) < k:
+              pre_topk.add(cur_rank)
+
+      # back testing on top k
+      return_ration_topk = 0
+      for index in pre_topk:
+          return_ration_topk += label[i][index]
+      return_ration_topk /= k
+      bt_k += return_ration_topk
+    return bt_k
